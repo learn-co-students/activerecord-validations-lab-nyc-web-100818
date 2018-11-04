14b5125b2d74636415bb28ac8666694029d2840c
Rexman17 <a.stone1794@gmail.com>
2018-11-04 13:40:22 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
+class ClickbaitynessValidator < ActiveModel::Validator
+  def validate(post)
+    if post.title
+      unless post.title.split(" ").include?("Believe" || "Secret" || "Top" || "Guess")
+        post.errors[:title] << 'Needs to be more clickbaity bro!'
+      end
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category"}
+  validates_with ClickbaitynessValidator
 end
