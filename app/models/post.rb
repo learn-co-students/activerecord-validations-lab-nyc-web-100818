8962d12eda53d1b68c78f11199a587adcd683543
magic8baller <lturis@gmail.com>
2018-11-04 21:55:45 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,20 +3,22 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
-  validates :clickbait, presence: true, clickbait: true
+  validates :clickbait
 
-end
 
 
 #You can pass more than one symbol for each class method and the respective validations will be run in the same order as they were registered.
 
 #The valid? method will verify that the errors collection is empty, so your custom validation methods should add errors to it when you wish validation to fail:
 
-class ClickbaitValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    value = ["Won't Believe", "Secret", "Top[number]", "Guess"]
-    unless value = value.any? {|v| attribute.include? (v) }
-      record.errors[attribute] << ("nobody puts clickbait in a corner")
+
+  def clickbait
+    bait = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+    #check inclusion
+    if title && !bait.any? { |bait|  title.include?(bait) }
+      #add errors 'if .. &&..'
+      errors.add(:title, "take me down a click-hole...")
     end
   end
+
 end
