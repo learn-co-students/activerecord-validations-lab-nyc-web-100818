6c5c770b3c9feb953f9014540e6083ef4cd5b9ce
yli0607x <github email address>
2018-11-05 19:01:39 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
+class ClickbaitValidator < ActiveModel::Validator
+  def validate(post)
+    if post.title
+      unless post.title.split(" ").include?("Believe" || "Secret" || "Top" || "Guess")
+        post.errors[:title] << 'False'
+      end
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validates_with ClickbaitValidator
+
+  # def clickbait(post)
+  #   if post.title.split(" ").!include?("Believe" || "Secret" || "Top" || "Guess")
+  #   post.errors[:title] << 'False'
+  #   end
+  # end
 end
